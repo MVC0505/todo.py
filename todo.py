474e87e811a9d8ddc0cf515a87b67fe2a409dd9c
MVC0505 <varunchandramittadoddi@gmail.com>
2025-11-16 17:38:54 +0530

    Add files via upload

---
+++ todo.py
@@ -0,0 +1,75 @@
+
+def load_tasks():
+    tasks = []
+    try:
+        with open(FILENAME, "r") as file:
+            for line in file:
+                tasks.append(line.strip())
+    except FileNotFoundError:
+        pass  # No tasks yet
+    return tasks
+
+
+def save_tasks(tasks):
+    with open(FILENAME, "w") as file:
+        for task in tasks:
+            file.write(task + "\n")
+
+def view_tasks(tasks):
+    if not tasks:
+        print("\nNo tasks found!\n")
+        return
+    
+    print("\nYour Tasks:")
+    for i, task in enumerate(tasks, 1):
+        print(f"{i}. {task}")
+    print()
+
+
+def add_task(tasks):
+    task = input("Enter new task: ")
+    tasks.append(task)
+    save_tasks(tasks)
+    print("Task added!\n")
+
+
+def remove_task(tasks):
+    view_tasks(tasks)
+    try:
+        task_no = int(input("Enter task number to remove: "))
+        if 1 <= task_no <= len(tasks):
+            removed = tasks.pop(task_no - 1)
+            save_tasks(tasks)
+            print(f"Removed task: {removed}\n")
+        else:
+            print("Invalid task number!\n")
+    except ValueError:
+        print("Please enter a valid number!\n")
+
+
+def main():
+    tasks = load_tasks()
+
+    while True:
+        print("=== TO-DO LIST APP ===")
+        print("1. View Tasks")
+        print("2. Add Task")
+        print("3. Remove Task")
+        print("4. Exit")
+
+        choice = input("Choose an option: ")
+
+        if choice == "1":
+            view_tasks(tasks)
+        elif choice == "2":
+            add_task(tasks)
+        elif choice == "3":
+            remove_task(tasks)
+        elif choice == "4":
+            print("Exiting... Goodbye!")
+            break
+        else:
+            print("Invalid choice! Try again.\n")
+
+if __name__ == "__main__":
+    main()
